b8464b8769a9b81a34c7bf6dee48d3182cbd49f1
dhanashreesp <124100015+dhanashreesp@users.noreply.github.com>
2023-02-12 22:59:58 -0800

    Created Counting, Compare & Multiplication Puzzle scenes
    Created Counting, Compare & Multiplication Puzzle scenes. In Couting Scene added drag and drop functionlity and in other screens will add Drag and Drop functionality in coming weeks

---
+++ Team3_KidsMathWithRabbit/Assets/CountingDragDrop.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CountingDragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject itemBeingDragged;
+    Vector3 startPosition;
+    Transform startParent;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+       // GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        itemBeingDragged = null;
+        //GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent != startParent)
+        {
+            transform.position = startPosition;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
