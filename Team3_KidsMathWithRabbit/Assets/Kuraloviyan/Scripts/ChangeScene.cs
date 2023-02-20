02d164501bc7849fb4dbce4f26d1995269ac3425
Imvkural-10 <008108705@coyote.csusb.edu>
2023-02-20 04:46:25 -0800

    Change Scene Script

--- Team3_KidsMathWithRabbit/Assets/Kuraloviyan/Scripts/ChangeScene.cs
+++ Team3_KidsMathWithRabbit/Assets/Kuraloviyan/Scripts/ChangeScene.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+<<<<<<< Updated upstream
 public class ChangeScene : MonoBehaviour
+=======
+public class ChangeofScene : MonoBehaviour
+>>>>>>> Stashed changes
 {
     // Start is called before the first frame update
     void CompareStart()
@@ -17,7 +21,11 @@ public class ChangeScene : MonoBehaviour
         
     }
 
+<<<<<<< Updated upstream
     public void nextScene(string Cname)
+=======
+    public void nextLoadScene(string Cname)
+>>>>>>> Stashed changes
     {
         SceneManager.LoadScene(Cname);
     }
